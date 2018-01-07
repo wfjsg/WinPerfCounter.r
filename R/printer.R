8dd28a48ede76760aa6cdc06a04a941b1e401ef4
Wataru Fujishige <fjsg@photonway.net>
2018-01-07 21:01:04 +0900

    分位点も出すようにした。

--- R/printer.R
+++ R/printer.R
@@ -10,8 +10,10 @@ WinPerfCounter.summary <- function(df) {
   statistics <- df %>% dplyr::group_by(metric) %>%
     dplyr::summarise_at(dplyr::vars(value), dplyr::funs(
       min(.,na.rm=T),
+      quantile25 = quantile(., na.rm=T, probs = 0.25),
       mean(.,na.rm=T),
       median(.,na.rm=T),
+      quantile75 = quantile(., na.rm=T, probs = 0.25),
       max(.,na.rm=T)))
   return(statistics);
 }
@@ -21,8 +23,10 @@ WinPerfCounter.summaryInGB <- function(df) {
   statistics <- df %>% dplyr::group_by(metric) %>%
     dplyr::summarise_at(dplyr::vars(valueInGB), dplyr::funs(
       min(.,na.rm=T),
+      quantile25 = quantile(., na.rm=T, probs = 0.25),
       mean(.,na.rm=T),
       median(.,na.rm=T),
+      quantile75 = quantile(., na.rm=T, probs = 0.75),
       max(.,na.rm=T)))
   return(statistics);
 }
@@ -31,8 +35,10 @@ WinPerfCounter.summaryInGB <- function(df) {
 WinPerfCounter.formattable <- function(df) {
   formattable::formattable(df, list(
     min = color_bar("aquamarine"),
+    quantile25 = color_bar("chartreuse"),
     mean = color_bar("chartreuse"),
     median = color_bar("steelblue"),
+    quantile75 = color_bar("chartreuse"),
     max = color_bar("chocolate")
   ))
 }
