f013de302e6e53c9ba2bf8a150791b51078e0356
Wataru Fujishige <fjsg@photonway.net>
2018-04-02 23:25:02 +0900

    simplied.

--- R/printer.R
+++ R/printer.R
@@ -59,13 +59,13 @@ WinPerfCounter.formattable <- function(df) {
 }
 
 #' @export
-WinPerfCounter.Process.PlotCPU <- function(cpu, processes, cpu.core, processName){
-  chart.conf <- processes[[processName]]
+WinPerfCounter.Process.PlotCPU <- function(cpu, plotConfig, processName){
+  chart.conf <- plotConfig$process[[processName]]
   cpu_chart <- ggplot(cpu, aes(x = Timestamp, y=value))
   cpu_chart <- cpu_chart + ggtitle(paste("Process(", processName, ") CPU", sep = ""))
   cpu_chart <- cpu_chart + geom_point(aes(colour = metric), alpha = 0.7)
   if( chart.conf$cpu.scale.auto ){
-    cpu_chart <- cpu_chart + ylim(0, 100 * sysenv.cpucore)
+    cpu_chart <- cpu_chart + ylim(0, 100 * plotConfig$cpucore)
   }else{
     cpu_chart <- cpu_chart + ylim(chart.conf$cpu.min, chart.conf$cpu.max)
   }
@@ -74,13 +74,13 @@ WinPerfCounter.Process.PlotCPU <- function(cpu, processes, cpu.core, processName
 }
 
 #' @export
-WinPerfCounter.Process.PlotMemory <- function(memory, processes, cpu.core, processName){
-  chart.conf <- processes[[processName]]
+WinPerfCounter.Process.PlotMemory <- function(memory, plotConfig, processName){
+  chart.conf <- plotConfig$process[[processName]]
   memory_chart <- ggplot(memory, aes(x = Timestamp, y=valueInGB))
   memory_chart <- memory_chart + ggtitle(paste("Process(", processName, ") Memory", sep = ""))
-  memory_chart <- memory_chart + geom_hline(yintercept = sysenv.memory, linetype="dashed", colour="b lue")
-  memory_chart <- memory_chart + annotate("text", label=paste(sysenv.memory, "GB Installed"),
-                                          x=viewStart, y=sysenv.memory, hjust = 0.0, vjust = -0.5)
+  memory_chart <- memory_chart + geom_hline(yintercept = plotConfig$memory, linetype="dashed", colour="b lue")
+  memory_chart <- memory_chart + annotate("text", label=paste(plotConfig$memory, "GB Installed"),
+                                          x=viewStart, y=plotConfig$memory, hjust = 0.0, vjust = -0.5)
   memory_chart <- memory_chart + geom_point(aes(colour = metric), alpha = 0.7)
   if( chart.conf$memory.scale.auto == FALSE ){
     memory_chart <- memory_chart + ylim(chart.conf$memory.min, chart.conf$memory.max)
@@ -90,8 +90,8 @@ WinPerfCounter.Process.PlotMemory <- function(memory, processes, cpu.core, proce
 }
 
 #' @export
-WinPerfCounter.Process.PlotIOPS <- function(iops, processes, cpu.core, processName){
-  chart.conf <- processes[[processName]]
+WinPerfCounter.Process.PlotIOPS <- function(iops, plotConfig, processName){
+  chart.conf <- plotConfig$process[[processName]]
   chart <- ggplot(iops, aes(x = Timestamp, y=value))
   chart <- chart + ggtitle(paste("Process(", processName, ") IOPS", sep = ""))
   chart <- chart + geom_point(aes(colour = metric), alpha = 0.7)
@@ -103,8 +103,8 @@ WinPerfCounter.Process.PlotIOPS <- function(iops, processes, cpu.core, processNa
 }
 
 #' @export
-WinPerfCounter.Process.PlotIOBytes <- function(iobytes, processes, cpu.core, processName){
-  chart.conf <- processes[[processName]]
+WinPerfCounter.Process.PlotIOBytes <- function(iobytes, plotConfig, processName){
+  chart.conf <- plotConfig$process[[processName]]
   chart <- ggplot(iobytes, aes(x = Timestamp, y=valueInMB))
   chart <- chart + ggtitle(paste("Process(", processName, ") IO Mega Byte/s", sep = ""))
   chart <- chart + geom_point(aes(colour = metric), alpha = 0.7)
