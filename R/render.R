c6cb9731951adbe7dba0f24d03146983378f6dcf
Wataru Fujishige <fjsg@photonway.net>
2018-04-23 23:51:00 +0900

    specify charset etc.

--- R/render.R
+++ R/render.R
@@ -4,8 +4,14 @@ file.arg.name <- "--file="
 script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
 script.basename <- dirname(script.name)
 mdpath <- paste(script.basename, '/../', "inst/rmarkdown/templates/simple/skeleton/skeleton.Rmd", sep = "")
-mdpath
 
-rmarkdown::render(mdpath, 
+myarg <- commandArgs(trailingOnly = TRUE)
+csvfile <- normalizePath(myarg[1])
+
+rmarkdown::render(mdpath,
+    encoding = "UTF-8",
     output_dir = getwd(),
-    output_file = "report.html")
+    output_file = "report.html",
+    params = list(csvfile = csvfile)
+    )
+
